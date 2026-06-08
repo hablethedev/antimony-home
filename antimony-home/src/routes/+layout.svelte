08aa1ac34b8a5831e6262f91e7b4efaf5a5c8899
vedal789 <meepydexter@gmail.com>
2026-06-08 07:02:49 +0100

    antimony favicon yay i win

--- antimony-home/src/routes/+layout.svelte
+++ antimony-home/src/routes/+layout.svelte
@@ -1,10 +1,11 @@
 <script>
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/logo.svg';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
+	<title>Antimony - Home</title> 
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
--- antimony-home/src/routes/+page.svelte
+++ antimony-home/src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>hi its me antimony</h1>
+<p>im so cool 🤑</p>
